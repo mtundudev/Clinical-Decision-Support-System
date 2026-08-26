1fa8862408021e8bd95345f9aa041962a3337084
DOTTO MTUNDU <dottomtundu9@gmail.com>
2026-08-27 00:58:28 +0300

    initial setup

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app=FastAPI(title="Clinical Decision Support System Backend")
+
+
+
+@app.get("test")
+def test():
+    return{
+        "message":"welcome to our clinical support system backend"
+    }
---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+#import your model here
+
+from app.core.database import Base
+
+
+
+__all__=["Base"]
